5bb65f5fa9b8fb263d8fe45221fdfbe7eace90d5
Danielle Reed <47546820+Danceratops@users.noreply.github.com>
2021-07-19 20:07:13 -0400

    add more sidebar options

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,12 @@ import SidebarOption from './SidebarOption';
 
 import AddIcon from '@material-ui/icons/Add';
 import InboxIcon from '@material-ui/icons/Inbox';
+import StarIcon from '@material-ui/icons/Star';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import SendIcon from '@material-ui/icons/Send';
+import NoteIcon from '@material-ui/icons/Note';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import './Sidebar.css';
 
@@ -15,7 +21,13 @@ function Sidebar(){
                 Compose
             </Button>
 
-            <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
+            <SidebarOption Icon={InboxIcon} title="Inbox" number={54} selected={true}/>
+            <SidebarOption Icon={StarIcon} title="Starred" number={3} />
+            <SidebarOption Icon={WatchLaterIcon} title="Snoozed" number={1} />
+            <SidebarOption Icon={LabelImportantIcon} title="Important" number={7} />
+            <SidebarOption Icon={SendIcon} title="Sent" number={19} />
+            <SidebarOption Icon={NoteIcon} title="Drafts" number={4} />
+            <SidebarOption Icon={ExpandMoreIcon} title="More" number={""} />
         </div>
     )
 }
